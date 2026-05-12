a4787d7a0c4af644611f9eb7742f5d25c501ddbf
losiek <imbierowicz.aleks@gmail.com>
2026-05-12 15:59:55 +0200

    poprawione

--- kalkulator.py
+++ kalkulator.py
@@ -2,25 +2,27 @@ import time
 import math
 
 x = input("wybierz swoją pierwszą liczbę ")
-    
+
 for i in range(len(x)):
     if x[i] not in "0123456789":
         print("to nie jest liczba")
         quit()
-    else:
-        x = int(x)
+
+x = int(x)
+
 print("wybierz swoje działanie")
 print("1: +")
 print("2: -")
 print("3: *")
 print("4: :")
 print("5: √")
+
 dzial = input()
 dzial = int(dzial)
 
 if dzial == 5:
-    print(math.sqrt(x))
-    exit()
+    print("twój wynik to", math.sqrt(x))
+    quit()
 
 y = input("wybierz swoją drugą liczbę ")
 
@@ -28,22 +30,25 @@ for i in range(len(y)):
     if y[i] not in "0123456789":
         print("to nie jest liczba")
         quit()
-    else:
-        y = int(y)
+
+y = int(y)
 
 time.sleep(1)
 
 if dzial == 1:
-    print("twój wynik to", int(x) + int(y))
+    print("twój wynik to", x + y)
 
-if dzial == 2:
-    print("twój wynik to", int(x) - int(y))
+elif dzial == 2:
+    print("twój wynik to", x - y)
 
-if dzial == 3:
-    print("twój wynik to", int(x) * int(y))
+elif dzial == 3:
+    print("twój wynik to", x * y)
 
-if dzial == 4:
-    print("twój wynik to", int(x) / int(y))
+elif dzial == 4:
+    if y == 0:
+        print("nie można dzielić przez 0")
+    else:
+        print("twój wynik to", x / y)
 
-if dzial > 5:
+else:
     print("błędne działanie")
