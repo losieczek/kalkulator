3d443a1e27ee2db2633355d654fe28003b7e2adf
losiek <imbierowicz.aleks@gmail.com>
2026-05-12 15:42:17 +0200

    sprawdzanie czy to jest liczba

--- kalkulator.py
+++ kalkulator.py
@@ -1,24 +1,43 @@
 import time
 import math
-x = input("wypierz swoją pięrwszą liczbę ")
-y = input("wypierz swoją drugą liczbę ")
+
+x = input("wybierz swoją pierwszą liczbę ")\
+    
+for i in range(len(x)):
+    if x[i] not in "0123456789":
+        print("to nie jest liczba")
+        quit()
+
+y = input("wybierz swoją drugą liczbę ")
+
+
+for i in range(len(y)):
+    if y[i] not in "0123456789":
+        print("to nie jest liczba")
+        quit()
 
 print("wybierz swoje działanie")
 print("1: +")
 print("2: -")
 print("3: *")
 print("4: :")
+
 dzial = input()
 dzial = int(dzial)
+
 time.sleep(1)
 
 if dzial == 1:
-    print("twój wynik to",int(x)+int(y))
+    print("twój wynik to", int(x) + int(y))
+
 if dzial == 2:
-    print("twój wynik to",int(x)- int(y))
+    print("twój wynik to", int(x) - int(y))
+
 if dzial == 3:
-    print("twój wynik to",int(x)*int(y))
+    print("twój wynik to", int(x) * int(y))
+
 if dzial == 4:
-    print("twój wynik to",int(x)/int(y))
+    print("twój wynik to", int(x) / int(y))
+
 if dzial > 4:
     print("błędne działanie")
