0e1819e973de72cf144844bd26fdd9611937fbe8
losiek <imbierowicz.aleks@gmail.com>
2026-05-12 15:55:15 +0200

    pierwiastki

--- kalkulator.py
+++ kalkulator.py
@@ -1,29 +1,35 @@
 import time
 import math
 
-x = input("wybierz swoją pierwszą liczbę ")\
+x = input("wybierz swoją pierwszą liczbę ")
     
 for i in range(len(x)):
     if x[i] not in "0123456789":
         print("to nie jest liczba")
         quit()
-
-y = input("wybierz swoją drugą liczbę ")
-
-
-for i in range(len(y)):
-    if y[i] not in "0123456789":
-        print("to nie jest liczba")
-        quit()
-
+    else:
+        x = int(x)
 print("wybierz swoje działanie")
 print("1: +")
 print("2: -")
 print("3: *")
 print("4: :")
-
+print("5: √")
 dzial = input()
 dzial = int(dzial)
+
+if dzial == 5:
+    print(math.sqrt(x))
+    exit()
+
+y = input("wybierz swoją drugą liczbę ")
+
+for i in range(len(y)):
+    if y[i] not in "0123456789":
+        print("to nie jest liczba")
+        quit()
+    else:
+        y = int(y)
 
 time.sleep(1)
 
@@ -39,5 +45,5 @@ if dzial == 3:
 if dzial == 4:
     print("twój wynik to", int(x) / int(y))
 
-if dzial > 4:
+if dzial > 5:
     print("błędne działanie")
